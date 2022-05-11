be545984bcd6e82b6e0b653a1477a9a1e364891b
alexhaiduk <alexhaiduk82@gmail.com>
2022-05-11 17:35:00 +0300

    solution

--- task02-slice.go
+++ task02-slice.go
@@ -1,11 +1,11 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	var startindex int = len(input) - 1
-	//result = input[0:len(input)]
-	//var value = input[0:len(input)]
-	for i := startindex; i >= 0; i-- {
-		result[startindex-i] = input[i]
+	result = make([]int64, len(input)+1)
+	var l int = 1
+	for i := 0; i < len(input); i++ {
+		result[i] = input[len(input)-l]
+		l++
 	}
-	return result
+	return
 }
